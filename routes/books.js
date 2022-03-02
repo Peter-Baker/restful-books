c617c58b8cc32c27d13183cf28fbcc54e26c8ab9
Peter Baker <pet01020@tjx.com>
2022-03-02 10:17:59 -0500

    Added books to show in html

--- routes/books.js
+++ routes/books.js
@@ -1,6 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
+const books = [
+  {
+    id: '1',
+    title: 'The Great Gatsby',
+    author: 'F. Scott Fitzgerald',
+    authorId: '1',
+    year: 1925,
+  },
+  {
+    id: '2',
+    title: 'Clifford, the Big Red Dog',
+    author: 'Norman Bridwell',
+    authorId: '2',
+    year: 1963,
+  },
+];
+
+// GET /books/ -> [ array of books ]
+router.get('/', (req, res) => {
+  res.json(books);
+});
+
+router.get('/', (req, res) => {
+  res.send('This should return an array of books (in JSON).');
+});
+
 router.get('/gatsby', (req, res) => {
   res.send('The Great Gatsby');
 });
